5c2bc2dd1604bc1ecb38465f477b8eccd19b9ab6
Fatih Topal <hemtopal@gmail.com>
2023-07-24 22:00:33 +0300

    US001_V1

---
+++ src/test/java/pages/Page.java
@@ -0,0 +1,23 @@
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class Page {
+    public Page(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    //fatih
+
+
+
+    // locateler icin herkese yer ayrilmali
+
+
+
+    //ahmet
+
+
+
+}
